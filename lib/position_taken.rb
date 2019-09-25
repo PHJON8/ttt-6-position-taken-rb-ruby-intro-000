8a6d746bf2f7658d93fdc16a1ec02061bf060f18
Jo Onuma <joimportant@yahoo.co.uk>
2019-09-25 15:35:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,14 @@
 # code your #position_taken? method here!
 def position_taken?(board,index) #method name and parameters
+<<<<<<< HEAD
   if board[index] == " " || board[index] ==  "" || board[index] == nil # to return false for " ", "" & nil
    return false
  else board[index] == "X" || board[index] == "O"  #return true for "X" & "O"
+=======
+  if board[index] == " " || "" ||nil # to return false for " ", "" & nil
+   return false
+ else board[index] == ("X" || "O")  #return true for "X" & "O"
+>>>>>>> 3fca978786aab888c949fba75995de7938568c74
    return true
   end
 end
